c87d85e6f32498fc1f282117a0f3e8d23edc9535
IsakEliH <isakelihauksson@gmail.com>
2026-03-12 11:37:20 +0000

    added some more tests to main

--- BST/main.py
+++ BST/main.py
@@ -2,13 +2,18 @@ from binary_search_tree import Pair, BinarySearchTree
 
 def test0():
     tree = BinarySearchTree()
+    print(tree.is_empty())
     tree.insert_key(20)
-    tree.insert_key(30)
+    tree.insert(Pair(30,"SHIT"))
     tree.insert_key(40)
     tree.insert_key(10)
     print(tree)
     print(tree.is_in(10))
     print(tree.is_in(50))
+
+    print(tree.is_empty())
+    print(tree.get(30))
+    print(tree.get(5))
     """
     # For testing _before/_after_/_first/_last
     for pair in tree:
