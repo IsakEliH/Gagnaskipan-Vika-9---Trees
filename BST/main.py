f4407aa5b975391d380a142bd3195f77eb44d359
IsakEliH <isakelihauksson@gmail.com>
2026-03-17 18:16:30 +0000

    some testing in main

--- BST/main.py
+++ BST/main.py
@@ -5,24 +5,34 @@ def test0():
     print(tree.is_empty())
     tree.insert_key(20)
     tree.insert(Pair(30,"SHIT"))
+    tree.insert_key(25)
     tree.insert_key(40)
     tree.insert_key(10)
     print(tree)
-    print(tree.is_in(10))
-    print(tree.is_in(50))
+    # print(tree.is_in(10))
+    # print(tree.is_in(50))
 
-    print(tree.is_empty())
-    print(tree.get(30))
-    print(tree.get(5))
-    """
-    # For testing _before/_after_/_first/_last
-    for pair in tree:
-        print(pair.key, end=' ')
-    print()
-    for pair in reversed(tree):
-        print(pair.key, end=' ')
-    print()
-    """
+    # print(tree.is_empty())
+    # print(tree.get(30))
+    # print(tree.get(5))
+    
+    # print("For testing _before/_after_/_first/_last")
+    # for pair in tree:
+    #     print(pair.pair.key, end=' ')
+    # print()
+    # for pair in reversed(tree):
+    #     print(pair.pair.key, end=' ')
+    # print()
+    
+    print(tree._first().pair.key)
+    print(tree._last().pair.key)
+
+    # move_key = tree._move_to_key(None, 30)
+    # print(move_key.pair.key)
+
+    print(tree._before(tree._last().parent).pair.key)
+    print(tree._after(tree._first().parent).pair.key)
+
 
 test0()
 
